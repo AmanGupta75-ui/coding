1d766cd4f581527d798e223c05c8a1f3c14a3ae6
YourName <ag4827550@gmail.com>
2025-10-02 21:51:55 +0530

    update

---
+++ src/amannnn.java
@@ -0,0 +1,5 @@
+public class amannnn {
+    public static void main(String[] args) {
+    System.out.println("hellooo" );
+}
+}
--- src/namannnn.java
+++
@@ -1,6 +0,0 @@
-public class namannnn {
-    public static void main(String[] args) {
-        System.out.println("hellooo" );
-    }
-
-}
