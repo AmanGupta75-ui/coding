7d0d9f6c979414178c4eb8751eba33a528081fe5
YourName <ag4827550@gmail.com>
2025-10-11 19:04:35 +0530

    update

---
+++ src/oops/first.java
@@ -0,0 +1,23 @@
+package oops;
+
+public class first {
+   public static void main(String[] args) {
+       student ans= new student();
+       System.out.println(ans.rollnumber);
+       System.out.println(ans.marks);
+       System.out.println(ans.name);
+       ans.rollnumber=12;
+       ans.marks=32.54f;
+       ans.name="aman";
+       System.out.println(ans.rollnumber);
+       System.out.println(ans.marks);
+       System.out.println(ans.name);
+
+
+    }
+}
+  class student{
+    int rollnumber;
+    String name;
+    float marks;
+  }
