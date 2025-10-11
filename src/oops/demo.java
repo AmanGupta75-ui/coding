1dcc0108c27dee47bc1dd35868caacc33c157064
YourName <ag4827550@gmail.com>
2025-10-11 19:31:27 +0530

    update

---
+++ src/oops/demo.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class demo {
+      public static void main(String[]args) {
+          student aman=new student();
+          aman.name="aman";
+          aman.rollnumber=221;
+          aman.marks=63.54f;
+
+          System.out.println(aman.name);
+          System.out.println(aman.rollnumber);
+          System.out.println(aman.marks);
+          student ayush=new student();
+          System.out.println(ayush.name);
+          System.out.println(ayush.rollnumber);
+          System.out.println(ayush.marks);
+
+    }
+}
