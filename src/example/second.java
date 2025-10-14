c2b12095fc5179e5833dfb16b79eee934c714fa7
YourName <ag4827550@gmail.com>
2025-10-14 19:25:53 +0530

    update

---
+++ src/example/second.java
@@ -0,0 +1,11 @@
+package example;
+
+public class second {
+     public static void main(String[] args) {
+
+          first aman= new first(14,"aman",false,"male");
+
+         System.out.println(first.population);
+
+    }
+}
