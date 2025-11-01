6ade0d8db2baa27509527c0e99ab8079302ba871
YourName <ag4827550@gmail.com>
2025-11-01 18:59:58 +0530

    update

---
+++ src/polymorphism/runtime/main.java
@@ -0,0 +1,12 @@
+package polymorphism.runtime;
+
+public class main {
+     public static void main(String[] args) {
+
+         shape s1= new shape();
+         s1.area();
+         triangle t1= new triangle();
+         t1.area();
+
+    }
+}
