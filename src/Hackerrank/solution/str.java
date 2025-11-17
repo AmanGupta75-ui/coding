9fc98bfcfe5a6c77039d388120bc1f8f46d33633
YourName <ag4827550@gmail.com>
2025-11-17 23:54:39 +0530

    update

---
+++ src/Hackerrank/solution/str.java
@@ -0,0 +1,29 @@
+package Hackerrank.solution;
+import java.util.*;
+
+    public class str {
+
+        public static  void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            String A=sc.next();
+
+
+            /* Enter your code here. Print output to STDOUT. */
+            System.out.println((A.length() <= 50 && A.matches("[a-z]+"))?"No":"Yes");
+        }
+    }
+
+
+
+    class m{
+
+        int Add;
+
+        void main(int add) {
+            Add = add;
+        }
+    }
+
+
+
