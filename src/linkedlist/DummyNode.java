945ac8e4d6140df8447e3b9d3893680364a72dc6
YourName <ag4827550@gmail.com>
2026-03-18 19:13:49 +0530

    update

---
+++ src/linkedlist/DummyNode.java
@@ -0,0 +1,25 @@
+package linkedlist;
+
+public class DummyNode {
+    
+
+    while(curr.next!=null){
+        if(curr.next.val=val){
+            curr.next=curr.next.next;
+
+        }
+        else{
+            curr curr.next;
+
+        }
+    }
+    return DummyNode.next;
+
+
+
+
+     public static void main(String[] args) {
+
+     }
+
+}
--- src/linkedlist/main.java
+++ src/linkedlist/main.java
@@ -13,6 +13,25 @@ class Node{
 }
 
 public class main {
+    public static Node insertathead(Node head){
+        if(head== null)return new Node(55);
+
+        Node newHead=new Node(55);
+        newHead.next=head;
+        
+        return newHead;
+        
+    }
+    public static  Node insertatend(Node head){
+        if(head==null)return null;
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        Node n= new Node(45);
+        temp.next=n;
+        return head;
+    }
 
 
 
@@ -36,6 +55,38 @@ public class main {
         }
         return head;
 
+    }
+    public static Node insertatkposi(Node head, int k){
+
+
+        if(head==null){
+            if(k==1){
+                Node n=new Node(99);
+                return n;
+            }else{
+                return  null;
+            }
+        }
+        if(k==1){
+            Node n=new Node(99);
+            n.next= head;
+            return  n;
+        }
+
+        Node temp=head;
+        int count= 0;
+        while(temp!=head){
+            count++;
+             if(count==k-1){
+                 Node n= new Node(99);
+                 n.next= temp.next;
+                 temp.next=n;
+                 break;
+
+             }
+             temp= temp.next;
+        }
+        return head;
     }
 
     public static Node removeByValue(Node head,int value)
@@ -63,6 +114,7 @@ public class main {
 
 
 
+
     public static Node convertToLL(int [] arr) {
         int n = arr.length;
         Node n1 = new Node(arr[0]);
@@ -86,15 +138,23 @@ public class main {
     {   Scanner sc = new Scanner(System.in);
         int[] arr = {12,23,3,1,5,7,3};
         Node head = convertToLL(arr);
-        display(head);
-        System.out.println();
+//        display(head);
+//        System.out.println();
 
 
         //  int k = sc.nextInt();
-        int value = sc.nextInt();
+//int value = sc.nextInt();
         // head = removeKthPosition(head,k);
-        head = removeByValue(head,value);
+//        head = removeByValue(head,value);
+//        display(head);
+//        head= insertathead(head);
+//        display(head);
+//
+//        head=insertatend(head);
+//        display(head);
+        head=insertatkposi(head, k);
         display(head);
+
     }
 }
 
